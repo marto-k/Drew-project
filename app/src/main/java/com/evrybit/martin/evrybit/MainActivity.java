dccbe608c836320747ee5f9ccba8ebad3d10b513
marto-k <martow17@gmail.com>
2016-05-05 13:10:06 +0300

    add A

---
+++ app/src/main/java/com/evrybit/martin/evrybit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.evrybit.martin.evrybit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
