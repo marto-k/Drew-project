0569ece57dcec8cd00b177763f37b083371857bd
marto-k <martow17@gmail.com>
2016-05-20 21:40:22 +0300

    add evrybit

--- app/src/main/java/com/evrybit/martin/evrybit/Add_a_bit.java
+++ app/src/main/java/com/evrybit/martin/evrybit/Add_a_bit.java
@@ -3,11 +3,11 @@ package com.evrybit.martin.evrybit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Add_a_bit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_a_bit);
     }
 }
--- app/src/main/java/com/evrybit/martin/evrybit/Photo_viewport.java
+++ app/src/main/java/com/evrybit/martin/evrybit/Photo_viewport.java
@@ -3,11 +3,11 @@ package com.evrybit.martin.evrybit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Photo_viewport extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_photo_viewport);
     }
 }
